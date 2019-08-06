dcb0f10cb82dcbbd267a18251f37cbe594455295
grejerdev <gregory.je@gmail.com>
2019-08-06 22:35:49 +0300

    small changes

--- api/models/training.js
+++ api/models/training.js
@@ -1,24 +1,19 @@
 'use strict';
 
-module.exports = {
-    create: function (training) {
-        let new_training = {
-            id : "",
-            name: "",
-            description: "",
-            exercises: []
-        }
-        if (!training) {
-            return new_training; 
-        }
+module.exports = class Training{
+    id :string  = "";
+    name:string = "";
+    description:string = "";
+    exercises: Array<any> = null;
 
-        new_training.id =training.id;
-        new_training.name =training.name;
-        new_training.description = training.description;
-        new_training.exercises =training.exercises 
-        return new_training;
-    },
-    parseToResponse:(training)=>{
+    constructor(training){
+        this.id = training.id;
+        this.name = training.id;
+        this.description =training.id;
+        this.exercises = training.id;
+    }
+
+    parseToResponse(training){
         return training;
     }
 }
