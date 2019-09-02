9ed2f0d086f1756af999c3a51a092d81d5717c77
grejerdev <gregory.je@gmail.com>
2019-09-02 22:07:14 +0300

    typo fixes

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const express = require('express');
 const app = express();
 const load_routes = require('./api/routes/routes');
 const init_middleware = require('./api/middlewares/load-middlewares');
+var passport = require("passport"); // at header app.use(passport.initialize()); // after line no.20 (express.static)
+require("./config/passport");
 
 init_middleware(app);
 load_routes(app, express);
+app.use(passport.initialize());
 
 const port = 3000;
 app.listen(port);
